49e7f3b1c375a27fdba516d856f9676aa7986f8f
Andrii Babushko <andriibabushko@gmail.com>
2022-09-09 18:04:14 +0300

    Finish task 6

---
+++ Lab1/src/js/task6.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function enterNumber() {
+    let number;
+
+    do {
+        number = parseFloat(prompt('Щоб зупинити введення чисел введіть 0.\nВведіть число:'));
+    } while (checkNumber(number));
+
+    return number;
+}
+
+function checkNumber(number) {
+    if (isNaN(number)) {
+        alert('Число введено не правильно!');
+        return true;
+    }
+    return false
+}
+
+const questionToContinue = () => {
+    let answer = prompt('Чи бажаєте ви вирішити продовжити введення?\n(так/ні)');
+
+    switch (answer) {
+        case 'Так':
+        case 'так':
+            return true;
+        case 'Ні':
+        case 'ні':
+            return false;
+    }
+};
+
+const sumUntilNot0 = () => {
+    let sum = 0;
+    let number;
+    do {
+        number = enterNumber();
+        sum += number;
+    } while (number !== 0);
+
+    return sum;
+};
+
+do {
+    alert(`Сума введених чисел = ${sumUntilNot0()};`);
+} while (questionToContinue());
+
