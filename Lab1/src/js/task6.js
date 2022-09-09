653e64822706e255327263718d71825f0399f958
Andrii Babushko <andriibabushko@gmail.com>
2022-09-09 19:36:17 +0300

    Modify task 6 part 2

--- Lab1/src/js/task6.js
+++ Lab1/src/js/task6.js
@@ -31,18 +31,32 @@ const questionToContinue = () => {
     }
 };
 
+String.prototype.replaceAt = function(index, replacement) {
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+
+    return this.substring(0, index) + replacement + this.substring(index + 1);
+}
+
 const sumUntilNot0 = () => {
     let sum = 0;
     let number;
+    let sumNumbers = '';
     do {
         number = enterNumber();
+        sumNumbers += number.toString() + '+';
         sum += number;
     } while (number !== 0);
 
-    return sum;
+    sumNumbers = sumNumbers.replaceAt(sumNumbers.length - 1, '=');
+
+    console.log(sumNumbers + sum.toString());
+    return sumNumbers + sum.toString();
 };
 
 do {
-    alert(`Сума введених чисел = ${sumUntilNot0()};`);
+
+    alert(`Сума введених чисел: ${sumUntilNot0()};`);
 } while (questionToContinue());
 
