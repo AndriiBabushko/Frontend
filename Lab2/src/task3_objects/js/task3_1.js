f60975e6b32ec21992fa667b3ded51b52a42759a
Andrii Babushko <andriibabushko@gmail.com>
2022-09-22 10:41:50 +0300

    Update task 3.1 in Lab 2

--- Lab2/src/task3_objects/js/task3_1.js
+++ Lab2/src/task3_objects/js/task3_1.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const checkClassNameInput = (enteredClassName) => enteredClassName.search(' ') !== -1;
+
 const checkRepeatedClassNames = (className, classNames) => {
     for (let i = 0; i < classNames.length; i++) {
         if (classNames[i] === className) {
@@ -22,6 +24,13 @@ const addClass = (obj, newClass) => {
 let obj = {
     className: "open menu",
 };
-let newClass = prompt("Введіть ім'я класу:");
-let newObj = addClass(obj, newClass);
-alert(`Новий об'єкт: ${newObj.className}`);
+
+let newClass = prompt("Введіть ім'я класу для додавання:");
+if(checkClassNameInput(newClass)){
+    let newObj = addClass(obj, newClass);
+    alert(`Новий об'єкт: ${newObj.className}`);
+} else {
+    alert('Назву класу введено не правильно!')
+}
+
+
