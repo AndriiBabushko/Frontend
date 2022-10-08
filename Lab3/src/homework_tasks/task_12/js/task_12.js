47f27f44a3cedde3bca144804e3b879b59f5280a
Andrii Babushko <andriibabushko@gmail.com>
2022-10-08 11:47:39 +0300

    Complete homework task 12 in Lab 3

---
+++ Lab3/src/homework_tasks/task_12/js/task_12.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const buttonLike = document.querySelector('#buttonLike');
+buttonLike.addEventListener('click', () => {
+    buttonLike.value = `${parseInt(buttonLike.value) + 1}`;
+    buttonLike.innerHTML = `<img src="../../../../img/icons/racoon_animal_icon_123571.ico" alt="raccoon_ico" 
+                            class="button_section__raccoon_ico">
+                            Like ${buttonLike.value}`;
+});
