3d627d1d42bf7a1e54b11ce6c58b119dc478118a
Andrii Babushko <andriibabushko@gmail.com>
2022-10-20 10:20:10 +0300

    Complete task 2 in Lab 4

--- Lab4/src/lab_tasks/task_2/js/task_2.js
+++ Lab4/src/lab_tasks/task_2/js/task_2.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const getRandomInt= (fromNumber, toNumber) => Math.round((Math.random() * (toNumber -
+    fromNumber) + fromNumber));
+
+const paragraphs = document.querySelectorAll('.table__paragraph');
+for (let i = 0; i < paragraphs.length; i++) {
+    const generatedNumber = getRandomInt(-10, 10);
+    paragraphs[i].innerHTML = `${generatedNumber}`;
+    paragraphs[i].setAttribute('value', generatedNumber.toString())
+    paragraphs[i].addEventListener('click', (event) => {
+        const number = parseInt(event.target.getAttribute('value')) ** 2;
+        paragraphs[i].setAttribute('value', number.toString())
+        paragraphs[i].innerHTML = `${number}`;
+    });
+}
