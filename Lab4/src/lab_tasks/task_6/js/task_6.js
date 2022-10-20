f6315aaf6187c83fa72759663c6e11a3da75cc8a
Andrii Babushko <andriibabushko@gmail.com>
2022-10-20 17:40:33 +0300

    Complete task 6 in Lab 4

--- Lab4/src/lab_tasks/task_6/js/task_6.js
+++ Lab4/src/lab_tasks/task_6/js/task_6.js
@@ -1,26 +1,48 @@
 'use strict';
 
-const selectChooseImg = document.querySelector('#selectChooseImg');
+function removeItemFromArray(array, item) {
+    const itemIndex = array.indexOf(item);
 
-selectChooseImg.addEventListener('input', () => {
-    const img = selectChooseImg.value;
-    const outputImg = document.querySelector('.button_section__output_img');
+    if (itemIndex > -1)
+        array.splice(itemIndex, 1);
 
-    switch (img) {
-        case 'los_angeles': {
-            outputImg.style.background = "url('../../.././img/los-angeles.jpg') no-repeat center";
-            outputImg.style.backgroundSize = 'contain';
-            break;
-        }
-        case 'new_york': {
-            outputImg.style.background = "url('../../.././img/new-york.jpg') no-repeat center";
-            outputImg.style.backgroundSize = 'contain';
-            break;
-        }
-        case 'chicago': {
-            outputImg.style.background = "url('../../.././img/chicago.jpg')  no-repeat center";
-            outputImg.style.backgroundSize = 'contain';
-            break;
-        }
+    return array;
+}
+
+function addEventListenersToList(nodeList, elemsList) {
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].addEventListener('click', (event) => {
+            const target = event.target;
+            elemsList = removeItemFromArray(elemsList, target.textContent);
+            target.remove();
+        })
     }
+}
+
+const buttonAddElemList = document.querySelector('#buttonAddElemList');
+const buttonRemoveElemList = document.querySelector('#buttonRemoveElemList');
+
+const olList = document.querySelector('#olList');
+let olListElements = document.querySelectorAll('.ol_list__li_element');
+
+let elemsList = []
+for (let i = 0; i < olListElements.length; i++) {
+    elemsList.push(olListElements[i].textContent);
+}
+
+buttonAddElemList.addEventListener('click', () => {
+    const element = document.createElement('li');
+    element.innerHTML = `${elemsList.length + 1}`;
+    element.classList.add('ol_list__li_element');
+
+    elemsList.push((elemsList.length + 1).toString());
+    olList.appendChild(element);
+
+    olListElements = document.querySelectorAll('.ol_list__li_element');
+    addEventListenersToList(olListElements, elemsList);
+});
+
+buttonRemoveElemList.addEventListener('click', () => {
+    olListElements[elemsList.length - 1].remove()
+    elemsList.pop();
 });
