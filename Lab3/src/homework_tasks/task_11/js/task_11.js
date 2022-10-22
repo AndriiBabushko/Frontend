384aa0f903e52f3932f3fed694526c271719ffb8
Andrii Babushko <andriibabushko@gmail.com>
2022-10-22 11:16:05 +0300

    Complete task 8 in Lab 4

--- Lab3/src/homework_tasks/task_11/js/task_11.js
+++ Lab3/src/homework_tasks/task_11/js/task_11.js
@@ -1,14 +1,37 @@
 'use strict';
 
 const progressBar = document.querySelector('#progressBar');
+
 const buttonAddProgressBar = document.querySelector('#buttonAddProgressBar');
+
+const checkProgressValue = () => {
+    if (progressBar.value < 25) {
+        console.log('<25');
+        // let n = 2 * parseInt(progressBar.getAttribute("value"));
+        // progressBar.style.setProperty("--c", "rgb("+n+","+n+",20)");
+        progressBar.style['-webkit-progress-value'].backgroundColor = 'red';
+        return;
+    }
+    if (progressBar.value >= 25 && progressBar.value <= 75) {
+        console.log('<=75');
+        // let n = 2 * parseInt(progressBar.getAttribute("value"));
+        // progressBar.style.setProperty("--c", "rgb("+n+","+n+",20)");
+        progressBar.style['-webkit-progress-value'].backgroundColor = 'orange';
+        return;
+    }
+    // let n = 2 * parseInt(progressBar.getAttribute("value"));
+    // progressBar.style.setProperty("--c", "rgb("+n+","+n+",20)");
+    progressBar.style['-webkit-progress-value'].backgroundColor = 'green';
+};
 
 buttonAddProgressBar.addEventListener('click', () => {
     progressBar.value += 5;
+    // checkProgressValue()
 });
 
 const buttonRemoveProgressBar = document.querySelector('#buttonRemoveProgressBar');
 
 buttonRemoveProgressBar.addEventListener('click', () => {
     progressBar.value -= 5;
+    // checkProgressValue()
 });
--- Lab4/src/homework_tasks/task_8/js/task_8.js
+++ Lab4/src/homework_tasks/task_8/js/task_8.js
@@ -1,9 +1,29 @@
 'use strict';
 
-const getRandomFloat = (fromNumber, toNumber) => Math.round((Math.random() * (toNumber - fromNumber) + fromNumber) * 100) / 100;
+const generateRandomColor = () => Math.floor(Math.random() * 16777215).toString(16);
 
-const buttonRandomNumber = document.querySelector('#buttonRandomNumber');
+const setEventsToBlocks = () => {
+    let blocks = document.querySelectorAll('.div_blocks_container__block');
 
-buttonRandomNumber.addEventListener('click', () => {
-    document.querySelector('.div_output_number__h3').innerHTML = getRandomFloat(-100, 100);
-})
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].addEventListener('click', (event) => {
+            event.target.remove();
+        });
+    }
+};
+
+const buttonNewBlock = document.querySelector('#buttonNewBlock');
+const blocksContainer = document.querySelector('.table__div_blocks_container');
+
+
+buttonNewBlock.addEventListener('click', () => {
+    const block = document.createElement('div');
+    const color = generateRandomColor();
+
+    block.classList.add('div_blocks_container__block');
+    block.style.backgroundColor = '#' + color;
+
+    blocksContainer.appendChild(block);
+    setEventsToBlocks();
+});
+
