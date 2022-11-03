60a7b9eaa2885f2699f7573f4a047e6ab06495c3
Andrii Babushko <andriibabushko@gmail.com>
2022-11-03 07:17:01 +0200

    Update task 1 in Lab 5

--- Lab5/src/lab_tasks/task_1/js/task_1.js
+++ Lab5/src/lab_tasks/task_1/js/task_1.js
@@ -1,8 +1,32 @@
 'use strict'
 
-const coordinatesContainer = document.querySelector('.coordinates_container');
+const findOutButtonClick = (button) => {
+    switch (button) {
+        case 0:
+            return 'Left button clicked.';
+        case 1:
+            return 'Middle button clicked.';
+        case 2:
+            return 'Right button clicked.';
+        case 3:
+            return 'Back button clicked.';
+        case 4:
+            return 'Forward button clicked.';
+        default:
+            return `Unknown button code: ${button}`;
+    }
+}
+
+const coordinates = document.querySelector('.coordinates');
+const mouse_click = document.querySelector('.mouse_click');
+
 document.querySelector('.wrapper').addEventListener('mousemove', (e) => {
-    const coordinateX = e.clientX
-    const coordinateY = e.clientY
-    coordinatesContainer.innerHTML = `X: ${coordinateX}; Y: ${coordinateY}`;
+    const coordinateX = e.clientX;
+    const coordinateY = e.clientY;
+    coordinates.innerHTML = `X: ${coordinateX}; Y: ${coordinateY}`;
 });
+
+window.addEventListener('mouseup', (e) => {
+    mouse_click.innerHTML = `${findOutButtonClick(e.button)}`
+})
+
