5c0ee5c56b47f6498742153f04b3100e7c75eb28
Andrii Babushko <andriibabushko@gmail.com>
2022-11-27 16:09:19 +0200

    Complete Lab 6-7 tasks

--- Lab6-7/src/task/php/login.php
+++ Lab6-7/src/task/php/login.php
@@ -14,9 +14,11 @@ $password = '';
     <title>Login</title>
     <link rel="icon" href="../../../../img/icons/racoon_animal_icon_123571.ico">
     <link rel="stylesheet" href="../styles/css/style.css?v=<?php echo time(); ?>">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous" defer></script>
 </head>
 <body>
 <?php include('navbar.php'); ?>
@@ -99,7 +101,8 @@ $password = '';
     </div>
 </main>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+<footer class="footer">
+    <?php include("footer.php") ?>
+</footer>
 </body>
 </html>
--- Lab6-7/src/task/php/register.php
+++ Lab6-7/src/task/php/register.php
@@ -22,6 +22,9 @@ $repeatPasswordCheck = false;
     <link rel="stylesheet" href="../styles/css/style.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous" defer></script>
 </head>
 <body>
 <?php include('navbar.php'); ?>
@@ -172,8 +175,9 @@ $repeatPasswordCheck = false;
     ?>
 </main>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+<footer class="footer">
+    <?php include("footer.php") ?>
+</footer>
 </body>
 </html>
 
